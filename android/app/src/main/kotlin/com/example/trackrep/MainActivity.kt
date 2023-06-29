5ea1fc6f5917d73a55cb2a462ecc794881bc3268
CodingCor <michael.mayer2105@gmail.com>
2023-06-29 21:13:30 +0200

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/trackrep/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trackrep
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:trackrep/pages/home.dart';
+
+Map<String, Widget Function(BuildContext)> routes = {
+  '/home' : (context) => const MyHomePage(title: "TrackRep"),
+};
+
+ThemeData data = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+  useMaterial3: true,
+);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TrackRep',
+      theme: data,
+      routes: routes,
+      initialRoute: '/home',
+    );
+  }
+}
+
