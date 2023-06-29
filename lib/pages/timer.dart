85d42f6a25daec6307dea27db2b7003f432d104b
CodingCor <michael.mayer2105@gmail.com>
2023-06-29 22:25:43 +0200

    Timer now counts up

--- lib/pages/timer.dart
+++ lib/pages/timer.dart
@@ -8,27 +8,70 @@ class Timer extends StatefulWidget {
 }
 
 class _TimerState extends State<Timer> {
+  
+  Stopwatch stopwatch = Stopwatch();
 
-  void resetStateIn(Duration dur){
-    Future.delayed(dur).then( (void para){
-      if(mounted){
-        debugPrint("Reset State");
-        setState((){}); 
-      }
-    });
+  @override 
+  void initState(){
+      super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    resetStateIn(const Duration(seconds: 1));
+    if(stopwatch.isRunning){
+      resetStateIn(const Duration(milliseconds: 100));
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Timer Example"),
       ),
       body: Center(
-        child: Text(DateTime.now().toString()),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(getTimeString()),
+            IconButton(
+              icon: const Icon(Icons.start),
+              onPressed: (){
+                restartStopwatch();
+              },
+            ),
+          ]
+        ),
       ),
     );
   }
+
+  void restartStopwatch(){
+    if(stopwatch.isRunning){
+      stopwatch.stop();
+    }else{
+      stopwatch.start();
+    }
+    setState((){});
+  }
+
+  void resetStateIn(Duration dur){
+    Future.delayed(dur).then( (void para){
+      if(mounted){
+        setState((){}); 
+      }
+    });
+  }
+
+  String getTimeString(){
+
+    String milliseconds = (stopwatch.elapsed.inMilliseconds % 1000).toString();
+    if(milliseconds.length > 2){
+      milliseconds = milliseconds.substring(0, 2);
+    }
+    milliseconds = milliseconds.padLeft(2, '0');
+
+    String seconds = (stopwatch.elapsed.inSeconds % 60).toString().padLeft(2, '0');
+    String minutes = (stopwatch.elapsed.inMinutes).toString().padLeft(2, '0');
+
+    return "$minutes:$seconds:$milliseconds";
+  }
+
 }
