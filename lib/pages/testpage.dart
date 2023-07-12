b5bce863b22d7e8b54102f13434cfca7d9b909cb
CodingCor <michael.mayer2105@gmail.com>
2023-07-12 22:19:52 +0200

    scroll test

--- lib/pages/testpage.dart
+++ lib/pages/testpage.dart
@@ -11,14 +11,19 @@ class _TestPageState extends State<TestPage> {
 
   @override
   Widget build(BuildContext context) {
+    int elementCount = 100;
+    List<Widget> items = [];
+    for(int i=0; i < elementCount; i++){
+      items.add(Card(child: Text(i.toString(), style: Theme.of(context).textTheme.displayMedium)));
+    }
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("TestPage"),
       ),
-      body: const Column(
-        children: <Widget>[
-        ]
+      body: ListWheelScrollView(
+        itemExtent: 100.0,
+        children: items,
       ),
     );
   }
