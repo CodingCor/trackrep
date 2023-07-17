cdc5b6954b169337ba51d80ae350ea70f858fe0f
CodingCor <michael.mayer2105@gmail.com>
2023-07-17 22:05:56 +0200

    rep picker layout

--- lib/pages/testpage.dart
+++ lib/pages/testpage.dart
@@ -13,6 +13,16 @@ class _TestPageState extends State<TestPage> {
   @override
   Widget build(BuildContext context) {
 
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("TestPage"),
+      ),
+      body: body(),
+    );
+  }
+
+  Widget body(){
     int elementCount = 100;
     List<Widget> items = [];
     for(int i=0; i < elementCount; i++){
@@ -22,31 +32,40 @@ class _TestPageState extends State<TestPage> {
         items.add(card(Colors.white, i));
       }
     }
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("TestPage"),
-      ),
-      body: ListWheelScrollView(
-        itemExtent: 120.0,
-        perspective: 0.000000000001,
-        children: items,
-        onSelectedItemChanged: (int value){
-          setState((){
-            selectedItem = value;
-          });
-        },
-      ),
+    return 
+    Column(
+      children: <Widget>[
+        const Spacer(),
+        Expanded( flex: 2, child: Padding(
+          padding: const EdgeInsets.all(12.0), 
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(flex: 2, child: Text("Push Ups", style: Theme.of(context).textTheme.displayMedium, textAlign: TextAlign.center)),
+              Expanded(flex: 1, child: 
+                ListWheelScrollView(
+                  itemExtent: 80.0,
+                  perspective: 0.000000000001,
+                  children: items,
+                  onSelectedItemChanged: (int value){
+                    setState((){
+                      selectedItem = value;
+                    });
+                  },
+                ),
+              ),
+            ]
+          )
+        )),
+        const Spacer(),
+      ],
     );
   }
 
   Card card(Color color, int index){
     return Card(
       color: color,
-      child: SizedBox(
-        width: 200.0,
-        child: Center(child: Text(index.toString(), style: Theme.of(context).textTheme.displayMedium)),
-      ),
+      child: Center(child: Text(index.toString(), style: Theme.of(context).textTheme.displayMedium)),
     );
   }
 }
