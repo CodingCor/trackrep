0eef0321512e26d8a7ee4c9ce852b7a577390085
CodingCor <michael.mayer2105@gmail.com>
2023-07-26 21:25:29 +0200

    testing possible program flow

--- lib/pages/testpage.dart
+++ lib/pages/testpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trackrep/widgets/number_picker.dart';
+import 'package:trackrep/widgets/timer.dart';
 
 class TestPage extends StatefulWidget {
   const TestPage({super.key});
@@ -10,6 +11,30 @@ class TestPage extends StatefulWidget {
 
 class _TestPageState extends State<TestPage> {
 
+
+  int currentPage = 0;
+  List<Widget> states =  [];
+
+  @override
+  void initState(){
+    super.initState();
+    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Hand Stand Push Up", onChoosen: nextPage));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
+    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Hand Stand Push Up", onChoosen: nextPage));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
+    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Hand Stand Push Up", onChoosen: nextPage));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
+    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Hand Stand Push Up", onChoosen: nextPage));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
+  }
+
+  void nextPage(){
+    currentPage++;
+    if(currentPage >= states.length) currentPage = states.length-1;
+    setState((){});
+  }
+
+
   @override
   Widget build(BuildContext context) {
 
@@ -18,7 +43,7 @@ class _TestPageState extends State<TestPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("TestPage"),
       ),
-      body: NumberPicker(text: "Pseudo Push Up", initalItem: 12, toNumber: 20, fromNumber: 2, onChoosen: (){print("return");}),
+      body: states[currentPage]
     );
   }
 }
