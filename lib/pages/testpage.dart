99e74c41d74d1835256199a0da5d8b98dec1a53c
CodingCor <michael.mayer2105@gmail.com>
2023-07-26 21:36:21 +0200

    test workout added

--- lib/pages/testpage.dart
+++ lib/pages/testpage.dart
@@ -18,14 +18,38 @@ class _TestPageState extends State<TestPage> {
   @override
   void initState(){
     super.initState();
-    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Hand Stand Push Up", onChoosen: nextPage));
-    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
-    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Hand Stand Push Up", onChoosen: nextPage));
-    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
-    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Hand Stand Push Up", onChoosen: nextPage));
-    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
-    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Hand Stand Push Up", onChoosen: nextPage));
-    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
+    // normal workout
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage, text: "Mountain Climbers"));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage, text: "Jumping Jacks"));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage));
+
+
+    for(int i = 0; i < 4; i++){
+      states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Shrimp Squat", onChoosen: nextPage));
+      states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
+    }
+
+    for(int i = 0; i < 4; i++){
+      states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wide Pull Up +14kg", onChoosen: nextPage));
+      states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 3), onSkip: nextPage));
+    }
+
+    for(int i = 0; i < 4; i++){
+      states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "V-Raise", onChoosen: nextPage));
+      states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Pseudo Push Up", onChoosen: nextPage));
+      states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1, seconds: 30), onSkip: nextPage));
+    }
+
+    states.add(NumberPicker(fromNumber: 0, toNumber: 12, text: "Wall Plank March", onChoosen: nextPage));
+
+    // stretching
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage, text: "Reach Down"));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage, text: "Stretch Back"));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 2), onSkip: nextPage, text: "Wall Sit"));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage, text: "Shoulder on Knee"));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage, text: "Shoulder Standing"));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage, text: "Hip Thrusters"));
+    states.add(Timer(resetable: false, skipable: true, finishTime: const Duration(minutes: 1), onSkip: nextPage, text: "Half Bridge"));
   }
 
   void nextPage(){
