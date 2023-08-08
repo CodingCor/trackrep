b2ae27bebfc67c41f8173e1ef815e453c29b90b6
CodingCor <michael.mayer2105@gmail.com>
2023-08-08 21:36:31 +0200

    Database class and TODOs

---
+++ lib/services/database.dart
@@ -0,0 +1,9 @@
+class Database{
+//TODO: initialize database here
+//      calls are to be made in their own service classes
+//      each model should get it's own model class to minimize file size
+//
+//TODO: how to handle automatically incrementing id's ?
+//
+//TODO: try creating a DB holding the created Model classes
+}
