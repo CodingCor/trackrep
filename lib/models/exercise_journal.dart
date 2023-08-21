937fd0cee9813c15b5cdb5c2995234fee9f5d42b
CodingCor <michael.mayer2105@gmail.com>
2023-08-21 21:55:46 +0200

    Exercise_log from and to map

--- lib/models/exercise_journal.dart
+++ lib/models/exercise_journal.dart
@@ -1,10 +1,58 @@
-import 'exercise.dart';
-
 class ExerciseJournalEntry{
-  int id;
-  DateTime time;
+  DateTime timestamp;
   int value;
-  Exercise exercise;
+  int exercise;
 
-  ExerciseJournalEntry({required this.id, required this.time, required this.value, required this.exercise});
+  ExerciseJournalEntry({required this.timestamp, required this.value, required this.exercise});
+
+  Map<String, String> toMap(){
+    Map<String, String> map = {};
+    map['logdate'] = _toDateString(timestamp);
+    map['logdate'] = _toTimeString(timestamp);
+    map['exercise'] = exercise.toString();
+    map['value'] = value.toString();
+    return map;
+  }
+
+  void fromMap(Map<String, String> data){
+    timestamp = _toTimeStamp(data['logdate'] ?? '', data['time'] ?? ''); 
+    value = int.tryParse(data['value'] ?? '') ?? 0;
+    exercise = int.tryParse(data['exercise'] ?? '') ?? 0; 
+  }
+
+  String _toDateString(DateTime stamp){
+    String year = stamp.year.toString().padLeft(4, '0');
+    String month = stamp.month.toString().padLeft(2, '0');
+    String day = stamp.day.toString().padLeft(2, '0');
+    return '$year-$month-$day';
+  }
+  String _toTimeString(DateTime stamp){
+    String hour = stamp.hour.toString().padLeft(2, '0');
+    String minute = stamp.minute.toString().padLeft(2, '0');
+    return '$hour:$minute';
+  }
+
+  DateTime _toTimeStamp(String date, String time){
+    List<String> dateSplit = date.split('-');
+    List<String> timeSplit = time.split(':');
+
+    int year = 1;
+    int month = 1;
+    int day = 1;
+    int hour = 1;
+    int minute = 1;
+
+    if(dateSplit.length == 3){
+      year =  int.tryParse(dateSplit[0]) ?? 1;
+      month = int.tryParse(dateSplit[1]) ?? 1;
+      day =   int.tryParse(dateSplit[2]) ?? 1;
+    }
+    if(timeSplit.length == 2){
+      hour =    int.tryParse(timeSplit[0]) ?? 1;
+      minute =  int.tryParse(timeSplit[1]) ?? 1;
+    }
+
+    return DateTime(year, month, day, hour, minute);
+  }
+
 }
