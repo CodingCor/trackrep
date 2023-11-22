bcd68a6c473adc9b78928b82032ee4acbfb72f67
CodingCor <michael.mayer2105@gmail.com>
2023-11-22 23:26:35 +0100

    workout exercise list rough structure

---
+++ lib/pages/workout_exercise.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:trackrep/models/exercise.dart';
+import 'package:trackrep/services/database.dart';
+
+class WorkoutExercisesPage extends StatefulWidget{
+  const WorkoutExercisesPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _WorkoutExercisesPageState();
+}
+
+class _WorkoutExercisesPageState extends State<WorkoutExercisesPage>{
+
+
+  List<Exercise> exercises = [];
+
+  @override
+  void initState(){
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Workout List"),
+      ),
+      body: exerciseListView(),
+      floatingActionButton: IconButton(
+        icon: const Icon(Icons.add),
+        onPressed: addButtonAction,
+      ),
+    );
+  }
+
+  Widget exerciseListView(){
+    return ListView.builder(
+      itemCount: exercises.length,
+      itemBuilder: (BuildContext context, int index){
+        return ListTile(
+          title: Text(exercises[index].name) 
+        );
+      }
+    );
+  }
+
+  void addButtonAction(){
+    //showDialog(context: context, builder: (BuildContext context){
+    //  return AlertDialog(
+    //    content: Column(
+    //      mainAxisSize: MainAxisSize.min,
+    //      children: <Widget>[
+    //        TextField(onChanged: (String value){text = value;}),
+    //        TextButton(onPressed: saveWorkout, child: const Text("Save")),
+    //      ],
+    //    ),
+    //  );
+    //});
+  }
+
+
+  void loadData() async {
+    setState((){});
+  }
+}
